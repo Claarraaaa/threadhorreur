40dd4feab77b204d20242134e900be872a29d025
unknown <clara.lanciano@my-digital-school.org>
2024-01-16 19:08:52 +0100

    Init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+/*import React from 'react' */
+
+/*const App = () =>*/
+export default function App() {
+  return (
+    <>
+        <header/>En-tête
+      <main>Parie Principale</main>
+      <footer> Copyright BDE XXX YYY </footer>
+    </>
+  )
+}
